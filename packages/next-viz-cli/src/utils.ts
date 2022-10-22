54c7e9173cae02530424d01630aff4097d57d5c4
Railly <railly_2009@hotmail.com>
2022-10-22 14:38:26 -0500

    feat(cli): Add base init command

--- packages/next-viz-cli/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const getVersion = () =>
-  fs
-    .readFileSync(path.join(__dirname, "../package.json"), "utf8")
-    .split("version")[1]
-    .split('"')[2];
